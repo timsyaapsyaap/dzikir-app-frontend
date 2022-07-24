64972c1ea51f2f0d875360adeebf59508057badb
Ahmad Aji Santoso <49109789+santoadji21@users.noreply.github.com>
2022-07-24 14:31:20 +0700

    Aji feature landing dekstop (#1)
    add landing page dekstop

---
+++ commitlint.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ], // allowed message examples: (feat: update some feature, chore: fix some style issue, etc)
+  },
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,34 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+
+// 2. Call `extendTheme` and pass your custom values
+const theme = extendTheme({
+  colors: {
+    dzikir: {
+      green: "#26A2B2",
+      bg: "#F4F2F9",
+      accentbg: "#E1EDF3",
+    },
+    text: {
+      body: "#515151",
+      black: "#212529",
+    },
+    fonts: {
+      body: "Poppins, sans-serif",
+      heading: "Poppins, sans-serif",
+      mono: "Poppins, monospace",
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
