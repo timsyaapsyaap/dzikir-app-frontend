6047e5af52328ecf2e6aad4c5c91cc93c3dd36c1
santoadji21 <ahmad.aji.santoso21@gmail.com>
2022-08-06 14:22:59 +0700

    fix: pwa bugs

--- next.config.js
+++ next.config.js
@@ -1,18 +1,31 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa");
-const runtimeCaching = require("next-pwa/cache");
+// const runtimeCaching = require("next-pwa/cache");
 
-const nextConfig = withPWA({
+// const nextConfig = withPWA({
+//   // reactStrictMode: true,
+//   // swcMinify: true,
+//   // pwa: {
+//   //   dest: "public",
+//   //   register: true,
+//   //   skipWaiting: true,
+//   //   runtimeCaching,
+//   //   disable: process.env.NODE_ENV === "development",
+//   // },
+// });
+
+// module.exports = nextConfig;
+
+// module.exports = withPWA({});
+
+const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   pwa: {
     dest: "public",
     register: true,
-    skipWaiting: true,
-    runtimeCaching,
-    disable: process.env.NODE_ENV === "development",
   },
-});
+};
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
